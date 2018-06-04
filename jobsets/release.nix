138eeedd6cad030d7e3a1e40ff966cf9cde3080e
Kris Jenkins <krisajenkins@gmail.com>
2018-06-04 10:20:55 +0100

    WIP: IEle builds - adding rvk.

--- jobsets/release.nix
+++ jobsets/release.nix
@@ -11,6 +11,9 @@ rec {
   secp256k1 = callPackage ./secp256k1.nix {
     inherit secp256k1Src;
   };
+  rvk = callPackage ./rvk.nix {
+    inherit rvkSrc;
+  };
   iele = callPackage ./iele.nix {
     inherit ieleSrc;
     inherit secp256k1;
