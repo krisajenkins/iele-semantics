8dc6f82ac089e33b85d6004cc57ad7444291ae07
Kris Jenkins <krisajenkins@gmail.com>
2018-06-05 12:27:09 +0100

    fixup! fixup! WIP: IELE Builds adding ocaml + k.

--- jobsets/ocaml-with-k.nix
+++ jobsets/ocaml-with-k.nix
@@ -9,7 +9,7 @@ pkgs.stdenv.mkDerivation {
 
   buildInputs = with pkgs; [ maven flex openjdk8 gcc ocaml_4_03 opam rsync curl unzip git m4 ];
 
-  buildPhase = "
+  buildPhase = ''
     export HOME="$NIX_BUILD_TOP"
     export NIX_CFLAGS_COMPILE="-Wno-error=unused-result $NIX_CFLAGS_COMPILE"
     mvn -Dmaven.repo.local=$HOME/.m2 package
@@ -20,7 +20,7 @@ pkgs.stdenv.mkDerivation {
 
     opam repository add k k-distribution/target/release/k/lib/opam
     opam switch 4.03.0+k
-  ";
+  '';
 
   installPhase = ''
     ls $out
